82147426ad59628bacbbc04c29b49c9d93ead874
Gus <gus@aufacicenta.com>
2021-02-04 14:02:40 -0600

    Translates current replies to ES

--- bot/src/i18n/commands/buy/es.ts
+++ bot/src/i18n/commands/buy/es.ts
@@ -1,16 +1,35 @@
 import { translationKeys } from "../../translationKeys";
 
 export default {
-  [translationKeys.buy_command_invalid_currency]: `El comando /compra funciona así:
+  [translationKeys.buy_command_invalid_currency]: `😖 No soportamos esta moneda, aún.
 
-<strong>/comprar [BTC ó ETH] [cantidad]</strong>
+El comando /comprar funciona así:
 
-Las <u>divisas disponibles</u> al momento son: <u>BTC y ETH</u>.`,
-  [translationKeys.buy_command_invalid_amount]: `El comando /compra funciona así:
+<code>/comprar [cantidad] [moneda]</code>
+<code>/comprar [cantidad] [moneda_origen/moneda_destino]</code>
 
-<strong>/comprar [BTC ó ETH] [cantidad]</strong>
+
+Ejemplo:
+
+Para comprar BTC por cualquier otra moneda:
+<code>/comprar 0.123 BTC</code>
+
+Para comprar BTC contra USD
+<code>/comprar 1 BTC/USD</code>`,
+  [translationKeys.buy_command_invalid_amount]: `El comando /comprar funciona así:
+
+<strong>/comprar [cantidad] [moneda_origen/moneda_destino]</strong>
 
 ¿Olvidaste la <u>cantidad</u>?`,
-  [translationKeys.buy_command_inline_markup_text_send_tx_id]: `Enviar ID de la transacción`,
-  [translationKeys.buy_command_inline_markup_text_attach_purchase_bank_note]: `Adjuntar recibo de pago`,
+  [translationKeys.buy_command_request_amount]: `Ok. ¿Cuánto?`,
+  [translationKeys.buy_command_request_currency]: `¿Qué moneda?`,
+  [translationKeys.buy_command_get_sell_orders_error]: `😖 Algo falló. Intenta de nuevo.`,
+  [translationKeys.buy_command_sell_orders]: `Estos usuarios están vendiendo ahora mismo:
+
+{{sell_orders_formatted}}
+Precio actual: {{price}}
+Fuente: {{- price_source}}
+
+<em>Negocia bajo tu propio riesgo. Aufa X Bot se milita a conectar compradores con vendedores.</em>
+`,
 };
