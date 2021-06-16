ff72ddf3fa81aec127a9f023c261238f50774af2
Ktoxcon <kennethtoxro@gmail.com>
2021-06-16 13:06:26 -0600

    feat: adding tests for country utilities

---
+++ bot/src/__test__/util/countries.test.ts
@@ -0,0 +1,31 @@
+import {
+  getCountryButtons,
+  getCountryFromName,
+} from "../../commands/util/countries";
+
+describe("Country Utilities", () => {
+  test("Country Utilities: Get Country Buttons", () => {
+    const countryButtons = getCountryButtons();
+
+    expect(countryButtons).not.toBeUndefined();
+    expect(countryButtons.length).not.toBeUndefined();
+    expect(countryButtons[0].length).toBeGreaterThan(0);
+    expect(countryButtons[0][0].text).not.toBeNull();
+    expect(countryButtons[0][0].text).not.toBeUndefined();
+  });
+
+  test("Country Utilities: Get Country From Name", () => {
+    const gtFullText = "Guatemala 🇬🇹";
+    const mxFullText = "Mexico 🇲🇽";
+
+    const mxInfo = getCountryFromName(mxFullText);
+    const gtInfo = getCountryFromName(gtFullText);
+
+    expect(mxInfo).not.toBeUndefined();
+    expect(gtInfo).not.toBeUndefined();
+    expect(mxInfo.name).toBe("Mexico");
+    expect(gtInfo.name).toBe("Guatemala");
+    expect(mxInfo.iso_alpha2).toBe("MX");
+    expect(gtInfo.iso_alpha2).toBe("GT");
+  });
+});
