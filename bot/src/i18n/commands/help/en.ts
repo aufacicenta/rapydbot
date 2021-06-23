2438cb52fcfd198df8ddfcc30bd048163a840eda
Gustavo Ibarra <gus@aufacicenta.com>
2021-06-23 11:15:43 -0500

    chore: updates /help language replies

--- bot/src/i18n/commands/help/en.ts
+++ bot/src/i18n/commands/help/en.ts
@@ -2,11 +2,9 @@ import { translationKeys } from "../../translationKeys";
 
 export default {
   [translationKeys.command_text_help]: `
-I can help you with the creation and 
-management of your Rapyd E-Wallet.
+I can help you with the creation and management of your Rapyd E-Wallet.
 
-You can control me by sending these 
-commands:
+You can control me by sending these commands:
 
 <b><u>Wallet Actions</u></b>
 <strong>/createwallet</strong> - create a new Rapyd e-wallet
@@ -15,11 +13,9 @@ commands:
 
 <strong>/send</strong> - send funds to other users on telegram
 
-<strong>/withdraw</strong> - withdraw cash from your wallet at Rapyd POS
-locations, such as ATMs and retail stores
+<strong>/withdraw</strong> - withdraw cash from your wallet at Rapyd POS locations, such as ATMs and retail stores
 
-<strong>/balance</strong> - get your e-wallet balance by your default 
-currency
+<strong>/balance</strong> - get your e-wallet balance by your default currency
 
 <b><u>Wallet Settings</u></b>
 <strong>/setcountry</strong> - set your default country information
--- bot/src/i18n/commands/help/es.ts
+++ bot/src/i18n/commands/help/es.ts
@@ -2,19 +2,15 @@ import { translationKeys } from "../../translationKeys";
 
 export default {
   [translationKeys.command_text_help]: `
-Puedo ayudarte con la creación y administración
-de tu billetera Rapyd.
+Puedo ayudarte con la creación y administración de tu billetera Rapyd.
 
-Puedes controlarme enviando cualquiera
-de los siguientes comandos:
+Puedes controlarme enviando cualquiera de los siguientes comandos:
 
 <b><u>Control de tu Billetera</u></b>
 <strong>/crearbilletera</strong> - crea una nueva billetera Rapyd
-<strong>/recargar</strong> - crea una nueva página de pago para recargar
-tu billetera
+<strong>/recargar</strong> - crea una nueva página de pago para recargar tu billetera
 <strong>/enviar</strong> - envía fondos a otros usuarios de telegram
-<strong>/retirar</strong> - retira efectivo de tu billetera desde cualquier
-punto POS de Rapyd, como cajeros automáticos y tiendas de conveniencia    
+<strong>/retirar</strong> - retira efectivo de tu billetera desde cualquier punto POS de Rapyd, como cajeros automáticos y tiendas de conveniencia
 <strong>/balance</strong> - obtén el balance de tu billetera Rapyd
 
 <b><u>Configuración de tu Billetera</u></b>
