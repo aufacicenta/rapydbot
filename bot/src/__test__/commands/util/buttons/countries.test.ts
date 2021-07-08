9187d884d483f99c6ca43e098f5420e4180d0def
Ktoxcon <kennethtoxro@gmail.com>
2021-07-08 13:52:18 -0600

    fix: fixes in country name utility

--- bot/src/__test__/commands/util/buttons/countries.test.ts
+++ bot/src/__test__/commands/util/buttons/countries.test.ts
@@ -1,4 +1,7 @@
-import { getCountryButtons, getCountryFromName } from "../../../../commands/util/buttons/countries";
+import {
+  getCountryButtons,
+  getCountryFromName,
+} from "../../../../commands/util/buttons/countries";
 
 describe("Country Utilities", () => {
   test("Country Utilities: Get Country Buttons", () => {
@@ -14,15 +17,24 @@ describe("Country Utilities", () => {
   test("Country Utilities: Get Country From Name", () => {
     const gtFullText = "Guatemala 🇬🇹";
     const mxFullText = "Mexico 🇲🇽";
+    const svFullText = "El Salvador 🇸🇻";
+    const abFullText = "Antigua and Barbuda 🇦🇬";
 
     const mxInfo = getCountryFromName(mxFullText);
     const gtInfo = getCountryFromName(gtFullText);
+    const svInfo = getCountryFromName(svFullText);
+    const abInfo = getCountryFromName(abFullText);
 
     expect(mxInfo).not.toBeUndefined();
     expect(gtInfo).not.toBeUndefined();
+    expect(svInfo).not.toBeUndefined();
+    expect(abInfo).not.toBeUndefined();
     expect(mxInfo.name).toBe("Mexico");
     expect(gtInfo.name).toBe("Guatemala");
+    expect(svInfo.name).toBe("El Salvador");
+    expect(abInfo.name).toBe("Antigua and Barbuda");
     expect(mxInfo.iso_alpha2).toBe("MX");
     expect(gtInfo.iso_alpha2).toBe("GT");
+    expect(svInfo.iso_alpha2).toBe("SV");
   });
 });
