4712ad7c7242b60cb3e7b0c5e916b2443525bdf8
Gustavo Ibarra <gus@aufacicenta.com>
2023-03-05 23:21:16 -0600

    feat(web): navbar icon interval

--- web/src/ui/navbar/Navbar.tsx
+++ web/src/ui/navbar/Navbar.tsx
@@ -6,9 +6,19 @@ import { Grid } from "ui/grid/Grid";
 import { Typography } from "ui/typography/Typography";
 
 export const Navbar: React.FC<NavbarProps> = ({ children, className }) => {
-  const [a, setA] = useState();
+  const [glyph, setGlyph] = useState("_");
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const glyphs = ["_", "√", "☻", "❖", "🅫", "♥", "₳", "¿", "0", "?"];
+
+    const interval = setInterval(() => {
+      setGlyph(glyphs[Math.floor(Math.random() * glyphs.length)]);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div className={clsx(styles["navbar"], className)}>
@@ -16,7 +26,7 @@ export const Navbar: React.FC<NavbarProps> = ({ children, className }) => {
         <Grid.Col>
           <div className={styles["navbar__left"]}>
             <Typography.Headline1 flat className={styles["navbar__title"]}>
-              INFORMER
+              INFORMER&nbsp;&nbsp;<span>{glyph}</span>
             </Typography.Headline1>
           </div>
         </Grid.Col>
